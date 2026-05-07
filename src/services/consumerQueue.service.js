28ed61bff8e85768a8ce7b77e0dcf72ab62c9515
buithiquyentran <buithiquyentran29112003@gmail.com>
2026-05-07 22:20:54 +0700

    feat: add consumeToQueueNormal method to messageService

--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -10,6 +10,16 @@ const messageService = {
       throw error;
     }
   },
+  // consummer to queue normal
+   consumeToQueueNormal: async (queueName) => {
+    try {
+      const { channel } = await connectToRabbitMQ();
+      await consumerQueue(channel, queueName);
+    } catch (error) {
+      console.error("Error in message service:", error);
+      throw error;
+    }
+  },
 };
 
 module.exports =  messageService ;
