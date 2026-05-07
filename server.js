1be83b6166260fa3a43547f0c998b95ae874549d
buithiquyentran <buithiquyentran29112003@gmail.com>
2026-05-07 22:20:54 +0700

    feat: add consumeToQueueNormal method to messageService

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
-const { consumeToQueue } = require("./src/services/consumerQueue.service");
+const { consumeToQueue, consumeToQueueNormal, consumeToQueueDLX } = require("./src/services/consumerQueue.service");
 
-const queueName = "test-topic";
-consumeToQueue(queueName)
+// const queueName = "test-topic";
+// consumeToQueue(queueName)
+//   .then(() => {
+//     console.log(`Started consuming messages from queue: ${queueName}`);
+//   })
+//   .catch((error) => {
+//     console.error("Error starting consumer:", error);
+//   });
+
+const queueName = "notificationQueue";
+consumeToQueueNormal(queueName)
   .then(() => {
-    console.log(`Started consuming messages from queue: ${queueName}`);
+    console.log(`Started consuming to queue normal`);
+  })
+  .catch((error) => {
+    console.error("Error starting consumer:", error);
+  });
+
+consumeToQueueDLX(queueName)
+  .then(() => {
+    console.log(`Started consuming to queue DLX`);
   })
   .catch((error) => {
     console.error("Error starting consumer:", error);
--- src/services/consumerQueue.service.js
+++ src/services/consumerQueue.service.js
@@ -10,6 +10,61 @@ const messageService = {
       throw error;
     }
   },
+  // case processing
+  consumeToQueueNormal: async (queueName) => {
+    try {
+      const { channel } = await connectToRabbitMQ();
+      const notiQueue = "notificationQueue";
+      setTimeout(() => {
+        channel.consume(notiQueue, (msg) => {
+          if (msg !== null) {
+            console.log("Received message:", msg.content.toString());
+            channel.ack(msg);
+          }
+        });
+      }, 5000);
+    } catch (error) {
+      console.error("Error in message service:", error);
+      throw error;
+    }
+  },
+  // case failed processing -> set up DLX
+  consumeToQueueDLX: async (queueName) => {
+    try {
+      const { channel } = await connectToRabbitMQ();
+      const notificationExchangeDLX = "notificationExDLX"; // notification direct exchange
+      const notificationRoutingKeyDLX = "notificationDLX";
+      const notiQueueDLX = "notificationQueueDLX";
+
+      await channel.assertExchange(notificationExchangeDLX, "direct", {
+        durable: true,
+      });
+      const queueResultDLX = await channel.assertQueue(notiQueueDLX, {
+        exclusive: false,
+      });
+      await channel.bindQueue(
+        queueResultDLX.queue,
+        notificationExchangeDLX,
+        notificationRoutingKeyDLX,
+      );
+      await channel.consume(
+        queueResultDLX.queue,
+        (msg) => {
+          if (msg !== null) {
+            console.log(
+              "This is a message from the DLX queue:",
+              msg.content.toString(),
+            );
+            channel.ack(msg);
+          }
+        },
+        (noAck = true),
+      );
+    } catch (error) {
+      console.error("Error in message service:", error);
+      throw error;
+    }
+  },
 };
 
-module.exports =  messageService ;
+module.exports = messageService;
